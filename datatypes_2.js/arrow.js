68e1d6a358c5c5671bdfd362a35d3b6075a09eb7
bigyanthapa <thapabigyan860@gmail.com>
2024-05-24 22:24:58 +0545

    learn

--- datatypes_2.js/arrow.js
+++ datatypes_2.js/arrow.js
@@ -1,3 +1,22 @@
-const username={
-    
-}
+// const user={
+//         username : "Bigyan Thapa -",
+//         price : 999,
+//         welcomemessage : function(){
+//             console.log(`${this.username}  welcome to the website`);
+//             console.log(this);
+
+           
+//             }
+//         }
+
+// console.log(this);
+
+
+
+// function chai(){
+//     console.log(this.username);
+// }
+// chai()
+
+const addTwo = (num1,num2) => num1*num2
+console.log(addTwo(10,30))
